260dcb0ac08d9dd9e590fb2d61a352bec940aa31
usagizhang <usagizhang@usagizhang-PC0.tencent.com>
2015-09-30 15:10:40 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Color Inverser
+
+A chrome extensions, inverse website color. It's Ugly but helpful to someone watching browser long time and feeling tired eyes.
+
+[!ScreenShot](img/screen.jpg)
---
+++ inverse_color.js
@@ -0,0 +1,6 @@
+  var $body = document.body;
+  if ( $body.classList.contains('color-inverse') ) {
+  	$body.classList.remove('color-inverse');
+  } else {
+  	$body.classList.add('color-inverse');
+  }
